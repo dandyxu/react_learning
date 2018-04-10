8733d9c5bd5f732716cc923d614c6c1325c264a2
dandyxu <dandyjefferson@gmail.com>
2018-04-11 00:15:25 +0100

    Finished parent/child component

--- bulletin-board/src/Board.js
+++ bulletin-board/src/Board.js
@@ -2,10 +2,40 @@ import React from 'react';
 import Note from './Note';
 
 class Board extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            notes : [
+                {
+                    id : 23,
+                    note : 'Dandy'
+                },
+                {
+                    id : 19,
+                    note : 'Wenting'
+                },
+                {
+                    id : 8,
+                    note : 'Xu'
+                }
+            ]
+        }
+        this.eachNote = this.eachNote.bind(this);
+    }
+
+    eachNote (note, i) {
+        return (
+            <Note key={i}
+                    index={i}>
+                    {note.note}
+            </Note>
+        )
+    }
+
     render() {
         return (
             <div className="board">
-                <Note></Note>
+                {this.state.notes.map(this.eachNote)}
             </div>
         )
     }
