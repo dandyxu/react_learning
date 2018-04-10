1fa7f1f663fb2d73992a8df80fbb0be2aa8ce9d0
dandyxu <dandyjefferson@gmail.com>
2018-04-10 23:32:59 +0100

    Added interactive events

--- bulletin-board/src/Note.js
+++ bulletin-board/src/Note.js
@@ -3,13 +3,29 @@ import FaPencil from 'react-icons/lib/fa/pencil';
 import FaTrash from 'react-icons/lib/fa/trash';
 
 class Note extends React.Component {
+    constructor(props) {
+        super(props);
+        // Bind `this`
+        this.edit = this.edit.bind(this);
+        this.remove = this.remove.bind(this);
+    }
+
+    edit() {
+        alert("editing note");
+    }
+
+    remove() {
+        alert("removing note");
+    }
+
+
     render() {
         return (
             <div className="note">
                 <p>Learn React</p>
                 <span>
-                    <button id="edit"><FaPencil /></button>
-                    <button id="remove"><FaTrash /></button>
+                    <button onClick={this.edit} id="edit"><FaPencil /></button>
+                    <button onClick={this.remove} id="remove"><FaTrash /></button>
                 </span>
             </div>
         )
