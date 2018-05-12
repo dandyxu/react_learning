9ad0bbd313d78d8275c0be5493b937e058b0c5cb
dandyxu <dandyjefferson@gmail.com>
2018-05-12 22:59:03 +0100

    Adding notes

--- bulletin-board/src/Board.js
+++ bulletin-board/src/Board.js
@@ -1,28 +1,48 @@
 import React from 'react';
 import Note from './Note';
+import FaPlus from 'react-icons/lib/fa/plus';
 
 class Board extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             notes : [
-                {
-                    id : 0,
-                    note : 'Dandy'
-                },
-                {
-                    id : 1,
-                    note : 'Wenting'
-                },
-                {
-                    id : 2,
-                    note : 'Xu'
-                }
+                // {
+                //     id : 0,
+                //     note : 'Dandy'
+                // },
+                // {
+                //     id : 1,
+                //     note : 'Wenting'
+                // },
+                // {
+                //     id : 2,
+                //     note : 'Xu'
+                // }
             ]
         }
+        this.add = this.add.bind(this);
         this.eachNote = this.eachNote.bind(this);
         this.update = this.update.bind(this);
         this.remove = this.remove.bind(this);
+        this.nextID = this.nextID.bind(this);
+    }
+
+    add(text) {
+        this.setState(preState => ({
+            notes: [
+                ...preState.notes,
+                {
+                    id: this.nextID(),
+                    note: text
+                }
+            ]
+        }))
+    }
+
+    nextID() {
+        this.uniqueID = this.uniqueID || 0;
+        return this.uniqueID++;
     }
 
     update(newText, i) {
@@ -56,6 +76,10 @@ class Board extends React.Component {
         return (
             <div className="board">
                 {this.state.notes.map(this.eachNote)}
+                <button onClick={this.add.bind(null, "New Note")}
+                                id="add">
+                            <FaPlus />
+                </button>
             </div>
         )
     }
